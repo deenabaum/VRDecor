39d573d5ae3f6b7be258bde2f4cf300d4974c594
deenabaum <dbaumlet@gmail.com>
2023-11-02 17:45:40 -0700

    particle effect when colors change

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static Material activeMaterial;
+    public static GameObject zapFX;
     [SerializeField] Material defaultMaterial;
 
     // Start is called before the first frame update
     void Start()
     {
         activeMaterial = defaultMaterial;
+        zapFX = this.gameObject.transform.GetChild(0).gameObject;
     }
 
     public void SwitchActiveMaterial(Material newMat)
     {
         activeMaterial = newMat;
     }
+
+    
 }
--- Assets/Scripts/SwitchMaterial.cs
+++ Assets/Scripts/SwitchMaterial.cs
@@ -7,6 +7,34 @@ public class SwitchMaterial : MonoBehaviour
 
     public void SwitchMaterialToActive()
     {
-        gameObject.GetComponent<Renderer>().material = GameManager.activeMaterial;
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            Material[] allMat = gameObject.GetComponent<Renderer>().materials;
+            Material[] newMats = new Material[renderer.materials.Length];
+            for (int i = 0; i < allMat.Length; i++)
+            {
+                if (allMat[i].name.Contains("change"))
+                {
+                    newMats[i] = GameManager.activeMaterial;
+                }
+                else
+                {
+                    newMats[i] = allMat[i];
+                }
+            }
+
+            renderer.materials = newMats;
+            GameManager.zapFX.transform.position = this.gameObject.transform.position;
+            ParticleSystem ps = GameManager.zapFX.GetComponent<ParticleSystem>();
+            ps.Emit(10);
+            
+        }
+        else
+        {
+            Debug.Log("Object is missing a Renderer component");
+        }
+
+
     }
 }
